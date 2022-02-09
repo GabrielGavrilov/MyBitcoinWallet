ee34bb87d4df5f383dd1a1b0cfb0ddade2688494
Gabriel Gavrilov <79603829+GabrielGavrilov@users.noreply.github.com>
2022-02-09 15:58:25 -0600

    Update introductionFrame.java

--- src/Gui/Frames/introductionFrame.java
+++ src/Gui/Frames/introductionFrame.java
@@ -13,10 +13,15 @@ public class introductionFrame extends JFrame implements ActionListener, MouseLi
 
     static JLabel alreadyHaveAnAccount = new JLabel();
     static JButton signupBtn = new JButton("Sign Up");
+    static JLabel signupText;
+    static JPanel errorPanel;
+    static JLabel errorMessage;
+
+    static signupPanel pSignup = new signupPanel();
 
     public introductionFrame() throws InterruptedException  {
 
-        this.setTitle("MyBitcoinWallet");
+        this.setTitle("Sign Up :: MyBitcoinWallet");
         this.setSize(400, 250);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         this.setResizable(false);
@@ -26,30 +31,37 @@ public class introductionFrame extends JFrame implements ActionListener, MouseLi
             LABELS
         */
 
-        JLabel signupText = new JLabel();
+        signupText = new JLabel();
         signupText.setText("Sign Up");
         signupText.setFont(new Font("ARIAL", Font.PLAIN, 23));
-        signupText.setBounds(150, 10, 400, 30);
+        signupText.setBounds(160, 10, 400, 30);
 
-        alreadyHaveAnAccount.setBounds(105, 136, 180, 30);
+        alreadyHaveAnAccount.setBounds(110, 136, 180, 30);
         alreadyHaveAnAccount.setText("Already have an account?");
         alreadyHaveAnAccount.setFont(new Font("ARIAL", Font.PLAIN, 16));
         alreadyHaveAnAccount.addMouseListener(this);
+
+        errorMessage = new JLabel();
+        errorMessage.setFont(new Font("ARIAL", Font.PLAIN, 17));
 
         /*
             PANELS
         */
 
-        signupPanel pSignup = new signupPanel();
+        errorPanel = new JPanel();
+        errorPanel.setBackground(new Color(255, 99, 71));
+        errorPanel.setBounds(10,10,370,65);
+        errorPanel.setLayout(new GridBagLayout());
 
         /*
             BUTTONS
         */
 
-        signupBtn.setBounds(105,175, 180, 25);
+        signupBtn.setBounds(110,175, 180, 25);
         signupBtn.setFont(new Font("ARIAL", Font.PLAIN, 15));
         signupBtn.setFocusable(false);
         signupBtn.addMouseListener(this);
+        signupBtn.addActionListener(this);
 
         /*
             ADDING
@@ -64,8 +76,33 @@ public class introductionFrame extends JFrame implements ActionListener, MouseLi
 
     }
 
+    void sendError(String errMessage) {
+
+        this.setSize(400, 325);
+
+        signupText.setBounds(160, 85, 400, 30);
+        pSignup.setBounds(0, 120, 400, 100);
+        alreadyHaveAnAccount.setBounds(110, 211, 180, 30);
+        signupBtn.setBounds(110,250, 180, 25);
+
+        this.add(errorPanel);
+        errorPanel.add(errorMessage);
+        errorMessage.setText(errMessage);
+
+    }
+
     @Override
     public void actionPerformed(ActionEvent e) {
+
+        if(e.getSource() == signupBtn) {
+
+            String signupEmail = pSignup.userEmailInput.getText();
+
+            if(!signupEmail.contains("@")) {
+                sendError("Must have a valid email!");
+            }
+
+        }
 
     }
 
