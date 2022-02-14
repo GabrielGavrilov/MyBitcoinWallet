2dbafd192b0abfefc8166adad37127ffad9cb031
Gabriel Gavrilov <79603829+GabrielGavrilov@users.noreply.github.com>
2022-02-13 18:07:34 -0600

    Update walletFrame.java

--- src/Gui/Frames/walletFrame.java
+++ src/Gui/Frames/walletFrame.java
@@ -4,8 +4,12 @@ import Gui.Panels.sideMenuPanel;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
-public class walletFrame extends JFrame {
+public class walletFrame extends JFrame implements ActionListener, MouseListener {
 
     /*
         PANELS
@@ -23,6 +27,15 @@ public class walletFrame extends JFrame {
         this.setResizable(false);
 
         /*
+            ADDING LISTENERS
+        */
+
+        sideMenu.menuOverview.addMouseListener(this);
+        sideMenu.menuSend.addMouseListener(this);
+        sideMenu.menuReceive.addMouseListener(this);
+        sideMenu.menuSettings.addMouseListener(this);
+
+        /*
             ADDING
         */
 
@@ -32,4 +45,69 @@ public class walletFrame extends JFrame {
 
     }
 
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+
+    @Override
+    public void mouseClicked(MouseEvent e) {
+
+        if(e.getSource() == sideMenu.menuSend) {
+
+            sideMenu.menuOverview.setBackground(new Color(9, 12, 20));
+            sideMenu.overviewLabel.setForeground(new Color(126, 133, 143));
+            sideMenu.menuOrangeOverview.setVisible(false);
+
+            sideMenu.menuSend.setBackground(new Color(19, 22, 31));
+            sideMenu.sendLabel.setForeground(Color.white);
+            sideMenu.menuOrangeSend.setVisible(true);
+
+            sideMenu.menuReceive.setBackground(new Color(9, 12, 20));
+            sideMenu.receiveLabel.setForeground(new Color(126, 133, 143));
+            sideMenu.menuOrangeReceive.setVisible(false);
+
+            sideMenu.menuSettings.setBackground(new Color(9, 12, 20));
+            sideMenu.settingsLabel.setForeground(new Color(126, 133, 143));
+            sideMenu.menuOrangeSettings.setVisible(false);
+
+        }
+
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+        if(e.getSource() == sideMenu.menuOverview) {
+            sideMenu.menuOverview.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        }
+
+        if(e.getSource() == sideMenu.menuSend) {
+            sideMenu.menuSend.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        }
+
+        if(e.getSource() == sideMenu.menuReceive) {
+            sideMenu.menuReceive.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        }
+
+        if(e.getSource() == sideMenu.menuSettings) {
+            sideMenu.menuSettings.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        }
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
