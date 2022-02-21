7c06d9df8baa3bd20d388c69f899194c2832c254
Gabriel Gavrilov <79603829+GabrielGavrilov@users.noreply.github.com>
2022-02-21 08:48:39 -0600

    update

--- src/Gui/Frames/loginFrame.java
+++ src/Gui/Frames/loginFrame.java
@@ -37,6 +37,7 @@ public class loginFrame extends JFrame implements ActionListener, MouseListener 
 
         login.loginButton.addActionListener(this);
         login.loginButton.addMouseListener(this);
+        login.dontHaveAnAccountLabel.addMouseListener(this);
 
         /*
             ADDING
@@ -49,6 +50,13 @@ public class loginFrame extends JFrame implements ActionListener, MouseListener 
     }
 
     public void sendError(String message) {
+
+        login.loginLabel.setBounds(115, 75, 300, 27);
+        login.emailInputLabel.setBounds(10,128,250,20);
+        login.emailInput.setBounds(10,150,278,27);
+        login.passwordInputLabel.setBounds(10,193,250,20);
+        login.passwordInput.setBounds(10,215,278,27);
+        login.dontHaveAnAccountLabel.setBounds(60,255,200,20);
 
         login.errorPanel.setVisible(true);
         login.errorMessage.setVisible(true);
@@ -81,6 +89,7 @@ public class loginFrame extends JFrame implements ActionListener, MouseListener 
 
             } catch(Exception err) {
 
+                sendError("There has been an issue login in.");
                 System.out.println(err);
 
             }
@@ -107,15 +116,28 @@ public class loginFrame extends JFrame implements ActionListener, MouseListener 
     @Override
     public void mouseEntered(MouseEvent e) {
 
+        if(e.getSource() == login.dontHaveAnAccountLabel) {
+
+            login.dontHaveAnAccountLabel.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+            login.dontHaveAnAccountLabel.setForeground(Color.white);
+
+        }
+
         if(e.getSource() == login.loginButton) {
+
             login.loginButton.setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
             login.loginButton.setBackground(new Color(209, 118, 8));
+
         }
 
     }
 
     @Override
     public void mouseExited(MouseEvent e) {
+
+        if(e.getSource() == login.dontHaveAnAccountLabel) {
+            login.dontHaveAnAccountLabel.setForeground(new Color(126, 133, 143));
+        }
 
         if(e.getSource() == login.loginButton) {
             login.loginButton.setBackground(new Color(247, 147, 26));
--- src/Gui/Panels/loginPanel.java
+++ src/Gui/Panels/loginPanel.java
@@ -19,6 +19,9 @@ public class loginPanel extends JPanel {
     */
 
     public JLabel errorMessage = new JLabel();
+    public JLabel loginLabel = new JLabel();
+    public JLabel emailInputLabel = new JLabel();
+    public JLabel passwordInputLabel = new JLabel();
     public JLabel dontHaveAnAccountLabel = new JLabel();
 
     /*
@@ -55,28 +58,25 @@ public class loginPanel extends JPanel {
         errorMessage.setFont(new Font("ARIAL", Font.PLAIN, 17));
         errorMessage.setForeground(Color.white);
 
-        JLabel loginLabel = new JLabel();
         loginLabel.setText("Log In");
         loginLabel.setFont(new Font("ARIAL", Font.BOLD, 25));
         loginLabel.setBounds(115, 25, 300, 27);
         loginLabel.setForeground(Color.white);
 
-        JLabel emailInputLabel = new JLabel();
         emailInputLabel.setText("Enter your login email:");
         emailInputLabel.setFont(new Font("ARIAL", Font.PLAIN, 16));
         emailInputLabel.setForeground(new Color(126, 133, 143));
         emailInputLabel.setBounds(10, 78, 250, 20);
 
-        JLabel passwordInputLabel = new JLabel();
         passwordInputLabel.setText("Enter your login password:");
         passwordInputLabel.setFont(new Font("ARIAL", Font.PLAIN, 16));
         passwordInputLabel.setForeground(new Color(126,133,143));
         passwordInputLabel.setBounds(10, 143, 250, 20);
 
         dontHaveAnAccountLabel.setText("Don't have an account?");
-        dontHaveAnAccountLabel.setFont(new Font("ARIAL", Font.PLAIN, 17));
+        dontHaveAnAccountLabel.setFont(new Font("ARIAL", Font.PLAIN, 18));
         dontHaveAnAccountLabel.setForeground(new Color(126,133,143));
-        dontHaveAnAccountLabel.setBounds(20, 200, 250, 20);
+        dontHaveAnAccountLabel.setBounds(60, 205, 200, 20);
 
         /*
             TEXT FIELDS
